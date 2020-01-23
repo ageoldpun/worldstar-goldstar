637ecba9d3110d263dd9cecd7641d1e2731a4bed
paul ogden <paul.ogden@nike.com>
2020-01-22 21:04:48 -0800

    WORLDSTAR STEP 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>WORLDSTAR GOLDSTAR</h1>
+        <img src="WorldStarGoldStarSmall.png" alt="WORLDSTAR GOLDSTAR" />
       </header>
+      <form>
+        <div>
+          <input type="text" id="yourName" />
+          <label for="yourName">Your Name</label>
+        </div>
+        <div>
+          <input type="text" id="theirName" />
+          <label for="theirName">Who you want to give a gold star</label>
+        </div>
+        <div>
+          <textarea id="whatTheyDid" rows="3"></textarea>
+          <label for="whatTheyDid">Why?</label>
+        </div>
+        <button type="submit">Send</button>
+      </form>
     </div>
   );
 }
